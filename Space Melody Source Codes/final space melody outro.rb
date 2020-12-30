55b8c7abe913c9917d60c83a7abfb0a2bd7821f1
Neeraj Mishra <xd1darkkn1ght1xd@gmail.com>
2020-12-31 00:09:24 +0530

    Update final space melody outro.rb

--- Space Melody Source Codes/final space melody outro.rb
+++ Space Melody Source Codes/final space melody outro.rb
@@ -1,5 +1,13 @@
-# outro with space melody.
+# Author: Neeraj Mishra
+# nmcnemis@gmail.com
+
+# outro for space melody.
+
 use_bpm 90
+
+# The sleep time is adjusted according to the music made with
+# "final space melody" file.
+# I played it after that music finished playing.
 1.times do
   play :D5, amp: 0.25, attack: 2, attack_level: 1, sustain: 4
   sleep 6
